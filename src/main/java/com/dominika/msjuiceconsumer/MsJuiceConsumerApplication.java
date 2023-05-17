380b4a58924a75414a37b83d2aa634583c59d7f8
dgoraj <dgoraj@deloittece.com>
2023-05-17 11:54:00 +0200

    initial commit

---
+++ src/main/java/com/dominika/msjuiceconsumer/MsJuiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dominika.msjuiceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJuiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJuiceConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominika/msjuiceconsumer/MsJuiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominika.msjuiceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJuiceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
