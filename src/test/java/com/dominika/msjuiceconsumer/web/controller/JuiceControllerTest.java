ac36eb3c19ae413d255a9a140a5bfb2ecf6b1017
dgoraj <dgoraj@deloittece.com>
2023-05-19 13:06:54 +0200

    partial changes

--- src/test/java/com/dominika/msjuiceconsumer/web/controller/JuiceControllerTest.java
+++ src/test/java/com/dominika/msjuiceconsumer/web/controller/JuiceControllerTest.java
@@ -2,17 +2,23 @@ package com.dominika.msjuiceconsumer.web.controller;
 
 import java.util.UUID;
 
+import com.dominika.msjuiceconsumer.services.JuiceService;
 import com.dominika.msjuiceconsumer.web.model.JuiceDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.RequestEntity.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
 @WebMvcTest(JuiceController.class)
 public class JuiceControllerTest {
 
@@ -22,9 +28,13 @@ public class JuiceControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    private JuiceService juiceService;
+
     @Test
     void getJuiceById() throws Exception{
- /*       mockMvc.perform(
+
+        mockMvc.perform((RequestBuilder)
             get("api/v1/juice/"+UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -34,7 +44,7 @@ public class JuiceControllerTest {
     void saveNewJuice() throws Exception{
         JuiceDto juiceDto = JuiceDto.builder().build();
         String juiceDtoJson = objectMapper.writeValueAsString(juiceDto);
-        mockMvc.perform(
+        mockMvc.perform((RequestBuilder)
             post("api/v1/juice/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(juiceDtoJson))
@@ -45,10 +55,10 @@ public class JuiceControllerTest {
     void updateJuiceById() throws Exception{
         JuiceDto juiceDto = JuiceDto.builder().build();
         String juiceToDtoJson = objectMapper.writeValueAsString(juiceDto);
-        mockMvc.perform(
+        mockMvc.perform((RequestBuilder)
             put("/api/v1/juice/"+UUID.randomUUID())
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(juiceToDtoJson))
-            .andExpect(status().isNoContent()); */
+            .andExpect(status().isNoContent());
     }
 }
